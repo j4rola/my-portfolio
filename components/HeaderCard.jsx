8dde7d17de176ab19730430ca8f6cc817f7fede0
Joe Froula <jmfroula@gmail.com>
2022-09-27 19:34:14 -0700

    initial

---
+++ components/HeaderCard.jsx
@@ -0,0 +1,18 @@
+import Card from 'react-bootstrap/Card';
+
+function HeaderCard() { 
+  return (
+    <Card className='my-4' style={{ width: '18rem' }}> 
+      <Card.Body className='d-flex flex-column align-items-center p-4'>
+        <Card.Title>Full Stack Web Developer</Card.Title>   
+        <Card.Text className='text-center'> 
+          I specialize in building solutions for small businesses that help them run more efficiently, and reach more people.
+        </Card.Text> 
+        <Card.Link href="mailto:jmfroula@gmail.com">Hire Me</Card.Link> 
+        
+      </Card.Body> 
+    </Card> 
+  );
+}
+
+export default HeaderCard;
