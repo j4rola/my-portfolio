3ff6789a35df9d93a613010b4db7f14188c6d50e
Joe Froula <jmfroula@gmail.com>
2022-09-29 16:40:04 -0700

    adds projects section

--- components/HeaderCard.jsx
+++ components/HeaderCard.jsx
@@ -1,16 +1,16 @@
-import Card from 'react-bootstrap/Card';
+import Card from 'react-bootstrap/Card'; 
 
 function HeaderCard() { 
-  return (
-    <Card className='my-4' style={{ width: '18rem' }}> 
-      <Card.Body className='d-flex flex-column align-items-center p-4'>
-        <Card.Title>Full Stack Web Developer</Card.Title>   
+  return ( 
+    <Card className='my-6 mx-6 border' style={{ width: '18rem' }}> 
+      <Card.Body className='d-flex flex-column align-items-center p-4'> 
+        <Card.Title>Full Stack Web Solutions</Card.Title>   
         <Card.Text className='text-center'> 
           I specialize in building solutions for small businesses that help them run more efficiently, and reach more people.
         </Card.Text> 
         <Card.Link href="mailto:jmfroula@gmail.com">Hire Me</Card.Link> 
         
-      </Card.Body> 
+      </Card.Body>   
     </Card> 
   );
 }
