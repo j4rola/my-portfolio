a575836b0ab07cff3bdc39fd3c04b2421f76af76
Joe Froula <jmfroula@gmail.com>
2022-10-11 10:57:48 -0700

    testing layout

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
 
       </section> 
 
-      <div className='flexy my-5'>
+      <div className='flexy my-5 py-5'>
         <div className='flexy small-custom-card d-md-none my-5'>
               
           <h1 className='text-blue text-center'>
