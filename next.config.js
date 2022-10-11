7f15418149b0f205a68f65e79600a8cdb1397d02
Joe Froula <jmfroula@gmail.com>
2022-10-11 12:25:50 -0700

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['https://asset.cloudinary.com/dcqmxufnx/4be0cceab1db347e7991fe849665d0fe'],
   },
