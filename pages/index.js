517b245295d740ec62fdb15c2819c0487a38e4cb
Joe Froula <jmfroula@gmail.com>
2022-10-25 13:20:17 -0700

    testing fetch

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,12 @@ export default function Home() {
   const handleText = async () => {  
     const data = await fetch('https://api.ipdata.co/?api-key=086f6229233c6b9511098c344b1594ec8f4dd4543448980e004b8518')
 
-    const res = axios.post('https://www.joefroula.dev/api/send-message', data)
+    //const res = axios.post('https://www.joefroula.dev/api/send-message', data)
+
+    const res = fetch('https://www.joefroula.dev/api/send-message', {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
   }
     
   handleText()
