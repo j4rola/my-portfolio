57b12bc8fb04b9780e8284db40aa185c8acb6307
Joe Froula <jmfroula@gmail.com>
2022-10-25 13:43:53 -0700

    testing add page

---
+++ pages/api/send-message.js
@@ -0,0 +1,35 @@
+const Vonage = require('@vonage/server-sdk')
+
+const vonage = new Vonage({
+  apiKey: "12550381",
+  apiSecret: "cSJPbQgwpcpWk6Tj"
+})
+
+const from = "18335789211"
+const to = "12037074230"
+
+//handle req 
+export default function handler(req, res) {  
+
+    const ip = req.body.data.ip
+    const domain = req.body.data.asn.domain
+    const city = req.body.data.city
+    const region = req.body.data.region 
+    const text = `Someone from ${city}, ${region} viewed your website. Domain: ${domain} IP Address: ${ip}`
+    
+    //create text message 
+    vonage.message.sendSms(from, to, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if(responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            }
+        }
+    })
+
+
+    res.status(200).json({ name: 'John Doe' }) 
+  }
