5915217d1ef81960538df991b3bf7d38e1028ec2
Joe Froula <jmfroula@gmail.com>
2023-03-08 09:53:26 -0800

    test

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
         <Skills></Skills>
       </section>
 
-      <iframe src="https://calendar.google.com/calendar/embed?src=1c1dfa4ce900c2a340bb24ce36acd5f9cfcb72a5306fa3de1d01cc580876c835%40group.calendar.google.com&ctz=America%2FLos_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+      <iframe src="https://calendar.google.com/calendar/embed?src=1c1dfa4ce900c2a340bb24ce36acd5f9cfcb72a5306fa3de1d01cc580876c835%40group.calendar.google.com&ctz=America%2FLos_Angeles" className="iframe-container"></iframe>
 
       <div className='ghost-y-100'></div>  
 
