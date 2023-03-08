ff5fbe9ec3a000f80b2133e91b2cf1a7e609720c
Joe Froula <jmfroula@gmail.com>
2023-03-08 11:56:33 -0800

    test

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,11 @@ export default function Home() {
 
   async function getData() {
     const data = await axios.get(URL)
+    const otherData = await axios.get('https://calendar.google.com/calendar/embed?src=1c1dfa4ce900c2a340bb24ce36acd5f9cfcb72a5306fa3de1d01cc580876c835%40group.calendar.google.com&ctz=America%2FLos_Angeles')
+    console.log(otherData)
     console.log(data)
   }
+
 
   getData()
 
