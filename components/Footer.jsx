5a94dbead98b353cb6c0a2c52483424a641d3ed1
Joe Froula <jmfroula@gmail.com>
2023-03-16 14:03:38 -0700

    test

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='blue text-light display-flex flex-column align-items-center justify-content-center h-100 p-5'>
-        <h3>© 2023 JM Consulting Co</h3>
+    <div className='blue text-light display-flex flex-column align-items-center h-100 p-5'>
+        
         <p>a thing</p>
         <p>a second thing</p>
+        <p>© 2023 JM Consulting Co</p>
     </div>
   )
 }
