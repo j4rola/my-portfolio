9643ebfc05497782bf4c5c8b9fdc6b7b5960c0cb
Joe Froula <jmfroula@gmail.com>
2023-04-19 20:33:51 -0700

    tweaks homepage

--- components/HeaderCard.jsx
+++ components/HeaderCard.jsx
@@ -7,7 +7,7 @@ function HeaderCard() {
   return ( 
     <Fade in timeout={{ enter: 3000 }}>
       <Card className='my-6 mx-6 text-blue p-5' style={{ width: '25rem' }}> 
-        <Card.Body className='d-flex flex-column align-items-center p-4'> 
+        <Card.Body className='d-flex flex-column align-items-center p-5'> 
           <Card.Title><h2 className='text-center'>Full Stack Web Solutions</h2></Card.Title>   
           <Card.Text className='text-center'> 
             I specialize in building solutions for small businesses that help them run more efficiently, and reach more people.
